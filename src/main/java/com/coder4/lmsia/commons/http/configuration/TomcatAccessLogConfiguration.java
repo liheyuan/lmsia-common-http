1eb00f8bfb3aee8c9dfc8c41718c8b2b6979df85
coder4 <lihy@fenbi.com>
2018-05-14 14:21:56 +0800

    MOD: init commit

---
+++ src/main/java/com/coder4/lmsia/commons/http/configuration/TomcatAccessLogConfiguration.java
@@ -0,0 +1,37 @@
+package com.coder4.lmsia.commons.http.configuration;
+
+import org.apache.catalina.valves.AccessLogValve;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author coder4
+ */
+@ConditionalOnWebApplication
+@Configuration
+public class TomcatAccessLogConfiguration
+        extends WebMvcConfigurerAdapter implements EmbeddedServletContainerCustomizer {
+
+    private Logger LOG = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        if (container instanceof TomcatEmbeddedServletContainerFactory) {
+            TomcatEmbeddedServletContainerFactory factory = (TomcatEmbeddedServletContainerFactory) container;
+            AccessLogValve accessLogValve = new AccessLogValve();
+            accessLogValve.setEnabled(true);
+            accessLogValve.setDirectory("/app/logs/");
+            accessLogValve.setPattern("common");
+            accessLogValve.setSuffix(".log");
+            factory.addContextValves(accessLogValve);
+        } else {
+            LOG.error("This customizer does not support your configured container!");
+        }
+    }
+}
