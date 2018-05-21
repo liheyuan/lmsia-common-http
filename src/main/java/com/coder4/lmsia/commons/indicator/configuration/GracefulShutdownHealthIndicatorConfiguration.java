5cdd233ed70f313211eb60d7cd90716c4fb4107a
coder4 <lihy@fenbi.com>
2018-05-21 19:33:49 +0800

    MOD: 0.0.3

--- src/main/java/com/coder4/lmsia/commons/indicator/configuration/GracefulShutdownHealthIndicatorConfiguration.java
+++ src/main/java/com/coder4/lmsia/commons/indicator/configuration/GracefulShutdownHealthIndicatorConfiguration.java
@@ -1,18 +1,19 @@
 package com.coder4.lmsia.commons.indicator.configuration;
 
 import com.coder4.lmsia.commons.indicator.GracefulStatusHealthIndicator;
-import org.springframework.beans.factory.DisposableBean;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PreDestroy;
 
 /**
  * @author coder4
  */
 @Configuration
 @ConditionalOnWebApplication
-public class GracefulShutdownHealthIndicatorConfiguration implements DisposableBean {
+public class GracefulShutdownHealthIndicatorConfiguration {
 
     private static final int GRACE_SHUTDOWN_MS = 3000;
 
@@ -23,7 +24,7 @@ public class GracefulShutdownHealthIndicatorConfiguration implements DisposableB
         return gshIndicator;
     }
 
-    @Override
+    @PreDestroy
     public void destroy() throws Exception {
         gshIndicator.setReady(false);
         Thread.sleep(GRACE_SHUTDOWN_MS);
