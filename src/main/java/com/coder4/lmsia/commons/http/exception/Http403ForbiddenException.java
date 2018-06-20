3af29d891f770812515c252ed0c51ccadf63b3d5
coder4 <lihy@fenbi.com>
2018-06-20 18:31:00 +0800

    ADD: exception

---
+++ src/main/java/com/coder4/lmsia/commons/http/exception/Http403ForbiddenException.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)Http413ForbiddenException.java, Jun 20, 2018.
+ * <p>
+ * Copyright 2018 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.coder4.lmsia.commons.http.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author coder4
+ */
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class Http403ForbiddenException extends RuntimeException {
+}
